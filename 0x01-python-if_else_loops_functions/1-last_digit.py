e042a05f1fe6ce89e5f784f3b4538bb5d0861b5e
yohannes soressa <ys10995@gmail.com>
2022-05-31 21:54:11 +0000

    number greater than 5 and less than 6

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
-new_number = str(number)
-if int(new_number[-1]) < 6 and number > 0:
-     print(f"Last digit of {number} is {new_number[-1]} and is less than 6 and not 0")
-elif number < 0 and int(new_number[-1]) != 0:
-     print(f"Last digit of {number} is {-int(new_number[-1])} and is less than 6 and not 0")
-elif int(new_number[-1]) > 5 and number > 0:
-     print(f"Last digit of {number} is {int(new_number[-1])} and is greater than 5")
+n = str(number)
+if int(n[-1]) < 6 and number > 0:
+    print(f"Last digit of {number} is {n[-1]} and is less than 6 and not 0")
+elif number < 0 and int(n[-1]) != 0:
+    print(
+        f"Last digit of {number} is {-int(n[-1])} and is less than 6 and not 0"
+    )
+elif int(n[-1]) > 5 and number > 0:
+    print(f"Last digit of {number} is {int(n[-1])} and is greater than 5")
 else:
-  print(f"Last digit of [number] is {int(new_number[-1])} and is 0")
+    print(f"Last digit of {number} is {int(n[-1])} and is 0")
