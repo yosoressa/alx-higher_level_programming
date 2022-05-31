b100391d53b878c9883759051d439828fc7d8756
yohannes soressa <ys10995@gmail.com>
2022-05-31 22:11:07 +0000

    number greater than 5 and less than 6

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,14 @@
 import random
 
 number = random.randint(-10000, 10000)
-n = str(number)
-if int(n[-1]) < 6 and number > 0:
-    print(f"Last digit of {number} is {n[-1]} and is less than 6 and not 0")
-elif number < 0 and int(n[-1]) != 0:
-    print(
-        f"Last digit of {number} is {-int(n[-1])} and is less than 6 and not 0"
-    )
-elif int(n[-1]) > 5 and number > 0:
-    print(f"Last digit of {number} is {int(n[-1])} and is greater than 5")
+new = abs(number) % 10
+if number < 0:
+    new = -new
+print("Last digit of {} is {} and is ".format(number, new), end="")
+if new > 5:
+    print("greater than 5")
+elif new == 0:
+    print("0")
 else:
-    print(f"Last digit of {number} is {int(n[-1])} and is 0")
+    print("less than 6 and not 0")
+
