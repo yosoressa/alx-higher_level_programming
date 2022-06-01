f0450a4de9bf4ac4300cb9ddaf6634e41497c61c
yohannes soressa <ys10995@gmail.com>
2022-06-01 06:32:43 +0000

    reverse alphabet

--- 0x01-python-if_else_loops_functions/11-main.py
+++ 0x01-python-if_else_loops_functions/11-main.py
@@ -5,4 +5,11 @@ print(pow(2, 2))
 print(pow(98, 2))
 print(pow(98, 0))
 print(pow(100, -2))
+print(pow(-4, 5)#!/usr/bin/env python3
+pow = __import__('11-pow').pow
+
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
 print(pow(-4, 5))
