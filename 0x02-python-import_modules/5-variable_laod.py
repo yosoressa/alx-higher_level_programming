6bc6522e23088514e55d52b392d6b90c29940015
yohannes soressa <ys10995@gmail.com>
2022-06-03 07:41:52 +0000

    variable load

--- 0x02-python-import_modules/5-variable_laod.py
+++ 0x02-python-import_modules/5-variable_laod.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def main():
-    import variable_load_5 as val
-    print(f"{val.a}")
-
 
 if __name__ == "__main__":
-    main()
+    from variable_load_5 import a
+
+    print(a)
